99b85832238d0b756adb8e80b0a1bf34126e4138
Gabriel Cândido <gabrielcandido234@gmail.com>
2025-11-05 17:14:45 -0300

    feat(firebase): initial firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    projectId: "teste-be220-38d66",
+    appId: "1:403196747477:web:44ee629b68b31b38118c5c",
+    storageBucket: "teste-be220-38d66.firebasestorage.app",
+    apiKey: "AIzaSyCgvV8ePYyZSsb1tEvR8HZoIUYPXujH64E",
+    authDomain: "teste-be220-38d66.firebaseapp.com",
+    messagingSenderId: "403196747477",
+    measurementId: "G-WF52M9BBEK",
+    projectNumber: "403196747477",
+    version: "2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    projectId: "teste-be220-38d66",
+    appId: "1:403196747477:web:44ee629b68b31b38118c5c",
+    storageBucket: "teste-be220-38d66.firebasestorage.app",
+    apiKey: "AIzaSyCgvV8ePYyZSsb1tEvR8HZoIUYPXujH64E",
+    authDomain: "teste-be220-38d66.firebaseapp.com",
+    messagingSenderId: "403196747477",
+    measurementId: "G-WF52M9BBEK",
+    projectNumber: "403196747477",
+    version: "2"
+  }
 };
 
 /*
